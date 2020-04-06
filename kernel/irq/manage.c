546652dfc2aa9e7e573080f31e5edd8a8177096a
Yaroslav Furman <yaro330@gmail.com>
2020-04-06 13:40:19 +0300

    kernel: irq: manage: use a different way of affining perf IRQs.
    Adjust cpumask directly.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: If92f2ebb3452095253c2d2f0869dde8f5bbed60a

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -1343,9 +1343,6 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 			if (ret)
 				goto out_thread;
 		}
-
-		if (new->flags & IRQF_PERF_CRITICAL)
-			affine_one_perf_thread(new->thread);
 	}
 
 	/*
